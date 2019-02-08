2add8dd6556676a54fc97f93cbf329f6e7a25db1
vasyl-ivanchuk <vasyl.ivanchuk@gmail.com>
2019-02-08 20:03:07 +0200

    Move API url to the configuration files

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import store from '@/store/store';
 
-const apiHost = process.env.VUE_APP_API_URL || 'http://localhost:3001';
+const apiHost = process.env.API_URL;
 export default () => axios.create({
   baseURL: `${apiHost}/`,
   headers: {
